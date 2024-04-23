9121d1365fa52a8ef1b59431902e3412c22844a6
Maestro1982 <kris.tote@telenet.be>
2024-04-23 11:16:00 +0200

    modify package.json, app.tsx in app admin folder, create list.tsx, edit.tsx and create.tsx in app admin challenge folder, create list.tsx, edit.tsx and create.tsx in app admin challengeOption folder, create edit.tsx, list.tsx and create.tsx in app admin lesson folder, create route.ts in app api challengeOptions folder, route.ts in app api challengeOptions [challengeOptionId] folder, route.ts in app api challenges folder, route.ts in app api challenges [challengeId] folder, route.ts in app api lessons folder, route.ts in app api lessons [lessonId] folder, prod.ts in scripts folder

--- app/admin/app.tsx
+++ app/admin/app.tsx
@@ -6,9 +6,22 @@ import simpleRestProvider from 'ra-data-simple-rest';
 import { CourseList } from './course/list';
 import { CourseCreate } from './course/create';
 import { CourseEdit } from './course/edit';
+
 import { UnitList } from './unit/list';
 import { UnitCreate } from './unit/create';
 import { UnitEdit } from './unit/edit';
+
+import { LessonList } from './lesson/list';
+import { LessonCreate } from './lesson/create';
+import { LessonEdit } from './lesson/edit';
+
+import { ChallengeList } from './challenge/list';
+import { ChallengeCreate } from './challenge/create';
+import { ChallengeEdit } from './challenge/edit';
+
+import { ChallengeOptionsList } from './challengeOption/list';
+import { ChallengeOptionsCreate } from './challengeOption/create';
+import { ChallengeOptionsEdit } from './challengeOption/edit';
 
 const dataProvider = simpleRestProvider('/api');
 
@@ -28,6 +41,28 @@ const App = () => {
         list={UnitList}
         create={UnitCreate}
         edit={UnitEdit}
+      />
+      <Resource
+        name='lessons'
+        recordRepresentation='title'
+        list={LessonList}
+        create={LessonCreate}
+        edit={LessonEdit}
+      />
+      <Resource
+        name='challenges'
+        recordRepresentation='title'
+        list={ChallengeList}
+        create={ChallengeCreate}
+        edit={ChallengeEdit}
+      />
+      <Resource
+        name='challengeOptions'
+        recordRepresentation='title'
+        list={ChallengeOptionsList}
+        create={ChallengeOptionsCreate}
+        edit={ChallengeOptionsEdit}
+        options={{ label: 'Challenge Options' }}
       />
     </Admin>
   );
